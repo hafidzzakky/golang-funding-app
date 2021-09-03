65a9cf650ae9c0a1b8e53067bd695654d44b9a72
hafidzzakkyd <hafidzzakkyd@gmail.com>
2021-09-03 15:54:08 +0700

    init project. Register user endpoint

---
+++ handler/user.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+	err := c.ShouldBindJSON(&input)
+
+	// Error validation handler
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// Send input data to service
+	newUser, err := h.userService.RegisterUser(input)
+	// Error sending
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	// Success sending
+	formatter := user.FormatUser(newUser, "xxxxxxxx")
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+}
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// DB Connection
+	dsn := "root:root@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// Test DB Conenction
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	// #Flow
+	// Input User Handler
+	// input user di proses ke service
+	// service diproses ke repository
+	// repository masuk ke db
+
+	// Router
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
