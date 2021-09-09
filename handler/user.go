481e60f695a74d519d14dc1fad2761ff01cb16da
hafidzzakkyd <hafidzzakkyd@gmail.com>
2021-09-09 14:18:48 +0700

    Add Validate Token

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwastartup/auth"
 	"bwastartup/helper"
 	"bwastartup/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -39,7 +41,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 	// Success sending
-	formatter := user.FormatUser(newUser, "xxxxxxxx")
+	token, err := h.authService.GenerateToken(newUser.ID)
+	// Error Generate Token
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, token)
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
@@ -64,7 +74,16 @@ func (h *userHandler) Login(c *gin.Context) {
 		return
 	}
 
-	loginFormatter := user.FormatUser(loggedInUser, "xxxxx")
+	// Success sending
+	token, err := h.authService.GenerateToken(loggedInUser.ID)
+	// Error Generate Token
+	if err != nil {
+		response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	loginFormatter := user.FormatUser(loggedInUser, token)
 	response := helper.APIResponse("Login successfully", http.StatusOK, "success", loginFormatter)
 	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/auth"
 	"bwastartup/handler"
 	"bwastartup/user"
 	"log"
@@ -22,8 +23,8 @@ func main() {
 
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
-	userHandler := handler.NewUserHandler(userService)
-
+	authService := auth.NewService()
+	userHandler := handler.NewUserHandler(userService, authService)
 	// #Flow
 	// Input User Handler
 	// input user diproses di service
